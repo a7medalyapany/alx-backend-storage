8402a49e63595ffa1611cd6b2034cdd3b3483949
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-05-15 04:41:01 +0300

    chore: Update log_stats.py to improve aggregation and print statements

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,25 +4,36 @@ from pymongo import MongoClient
 
 
 def log_stats():
-    # Connect to MongoDB
+    '''Connect to MongoDB'''
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
 
-    # Count total logs
-    total_logs = collection.count_documents({})
+    '''Pipeline for aggregation'''
+    pipeline = [
+        {"$group": {"_id": "$method", "count": {"$sum": 1}}},
+        {"$sort": {"_id": 1}}
+    ]
+
+    '''Execute aggregation'''
+    results = collection.aggregate(pipeline)
+
+    '''Print total logs'''
+    total_logs = sum(doc['count'] for doc in results)
     print(f"{total_logs} logs")
 
-    # Count methods
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+    '''Print methods count'''
+    results = collection.aggregate([
+        {"$group": {"_id": "$method", "count": {"$sum": 1}}},
+        {"$sort": {"_id": 1}}
+    ])
+    for doc in results:
+        print(f"\tmethod {doc['_id']}: {doc['count']}")
 
-    # Count status check
-    status_check = collection.count_documents(
+    '''Print status check count'''
+    status_check_count = collection.count_documents(
         {"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+    print(f"{status_check_count} status check")
 
 
 if __name__ == "__main__":
