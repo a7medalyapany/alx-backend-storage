8f77b2a8140e5e0ea190126bdba852a8b35616d7
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-05-16 03:59:23 +0300

    chore: Refactor Cache class to use type hints and improve code readability

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,6 +25,26 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator to store the history of inputs and outputs for a particular function.
+    """
+    inputs = f"{method.__qualname__}:inputs"
+    outputs = f"{method.__qualname__}:outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function for the decorator.
+        """
+        self._redis.rpush(inputs, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(result))
+        return result
+
+    return wrapper
+
+
 class Cache:
     """
     Cache class. Implements caching functions with a Redis client.
@@ -37,7 +57,6 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store the input data in Redis using a random key.
@@ -53,4 +72,18 @@ class Cache:
         result = self._redis.get(key)
         if fn:
             result = fn(result)
+        return result
+
+    def get_str(self, key: str) -> str:
+        """
+        Get the value of a string key from the Redis server and convert it to a string.
+        """
+        result = self.get(key, fn=str)
+        return result
+
+    def get_int(self, key: str) -> int:
+        """
+        Get the value of a string key from the Redis server and convert it to an integer.
+        """
+        result = self.get(key, fn=int)
         return result
