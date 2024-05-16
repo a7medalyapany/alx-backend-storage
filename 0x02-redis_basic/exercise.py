5987f2256b11632e8069d2476fce06db74c22cdf
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-05-16 03:36:33 +0300

    chore: Refactor Cache class to use type hints and improve code readability

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,34 +1,47 @@
 #!/usr/bin/env python3
-"""
+'''
 Main file
-"""
+'''
 import redis
 import uuid
 
 
 class Cache:
-    """
+    '''
     Cache class to store data in Redis
-    """
+    '''
 
-    def __init__(self):
-        """Initialize the Cache class with Redis client"""
+    def __init__(self) -> None:
+        '''Initialize the Cache class with Redis client'''
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
+        '''
         Store the input data in Redis using a random key and return the key
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        '''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
 
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+    ) -> Union[str, bytes, int, float]:
+        '''Gets a value
+        '''
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
 
-if __name__ == "__main__":
-    cache = Cache()
-    data = b"hello"
-    key = cache.store(data)
-    print(key)
-    local_redis = redis.Redis()
-    print(local_redis.get(key))
+    def get_str(self, key: str) -> str:
+        '''Gets a string
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        '''Gets an integer
+        '''
+        return self.get(key, lambda x: int(x))
